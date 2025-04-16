4512e71be55aad129ab716b947d2d57c9cca3393
AZIZImohammed <artazizi06@gmail.com>
2025-04-16 19:37:30 +0100

    Integrating Clay To the main Function

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,10 +2,15 @@
 #include "headers/Simulation.hpp"
 #include "clay.h"
 
+
 using namespace std;
 
 const int windowWidth = 1280;
 const int windowHeight = 720;
+
+void HandleClayErrors(Clay_ErrorData errorData) {
+    printf("%s", errorData.errorText.chars);
+}
 
 void DrawUI(Simulation sim) {
     // Title and instructions
@@ -23,19 +28,36 @@ void DrawUI(Simulation sim) {
 
 int main()
 {
+    // init clay
+    uint64_t totalMemorySize = Clay_MinMemorySize();
+    Clay_Arena arena = Clay_CreateArenaWithCapacityAndMemory(totalMemorySize, malloc(totalMemorySize));
+    Clay_Initialize(arena, (Clay_Dimensions) { windowWidth, windowHeight }, (Clay_ErrorHandler) { HandleClayErrors });
+
     InitWindow(windowWidth, windowHeight, "Automata Simulation");
     SetTargetFPS(60);
     Simulation simulation;
+  
+    ;
     while (!WindowShouldClose()) {
+
+
+        // Optional: Update internal layout dimensions to support resizing
+        Clay_SetLayoutDimensions((Clay_Dimensions) { GetScreenWidth(), GetScreenHeight() });
+        // Optional: Update internal pointer position for handling mouseover / click / touch events - needed for scrolling & debug tools
+        Clay_SetPointerState((Clay_Vector2) { GetMouseY(), GetMouseY() }, IsMouseButtonDown(MOUSE_BUTTON_LEFT));
+        // Optional: Update internal pointer position for handling mouseover / click / touch events - needed for scrolling and debug tools
+        Clay_UpdateScrollContainers(true, (Clay_Vector2) { 0.0, GetMouseWheelMove() },  GetFrameTime());
+
+        // All clay layouts are declared between Clay_BeginLayout and Clay_EndLayout
+        Clay_BeginLayout();
+        // implementing clay Tree Hierachy
+        Clay_EndLayout();
+
         BeginDrawing();
         simulation.update();
-
         ClearBackground(RAYWHITE);
-
         DrawUI(simulation);
-
         simulation.render();
-
         EndDrawing();
     }
 
---
+++ src/headers/Components.hpp
@@ -0,0 +1,7 @@
+#ifndef COMPONENTS_H
+#define COMPONENTS_H
+#include "clay.h"
+namespace com{
+    const Clay_Color COLOR_RED = (Clay_Color) {168, 66, 28, 255};
+}
+#endif
